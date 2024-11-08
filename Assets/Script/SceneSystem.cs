7b350124c9b7f90c921d8be75970f44e9e19c0f1
ivanma777 <ivamaruiz5604@gmail.com>
2024-11-08 11:17:14 +0100

    Menus

--- Assets/Script/SceneSystem.cs
+++ Assets/Script/SceneSystem.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneSystem : MonoBehaviour
 {
+
+    bool pause;
+
+    [SerializeField] GameObject MenuPause;
     // Start is called before the first frame update
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
 
     }
 
@@ -17,4 +23,49 @@ public class SceneSystem : MonoBehaviour
         Application.Quit();
 
     }
+
+    public void Pause()
+    {
+        if (pause )
+        {
+            Time.timeScale = 0f;
+            MenuPause.SetActive(true);
+
+        }
+
+    } 
+    public void Return()
+    {
+        if (!pause)
+        {
+
+            Time.timeScale = 1f;
+            MenuPause.SetActive(false);   
+        }
+
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+
+            Pause();
+            Return();
+            if (pause)
+            {
+                pause = false;
+            }
+            else
+            {
+
+                pause = true;
+            }
+           
+        }
+
+
+    }
+
+
 }
